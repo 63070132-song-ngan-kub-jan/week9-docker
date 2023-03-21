0697306a8953e97f7aa21bce226e95ef55801e27
Panupong Keawkam <63070132@kmitl.ac.th>
2023-03-21 21:48:35 +0700

    Add allow CORS in server

--- backend/main.py
+++ backend/main.py
@@ -6,8 +6,24 @@ import numpy as np
 import cv2
 import base64
 from typing import List
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8081",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class ImageRequest(BaseModel):
